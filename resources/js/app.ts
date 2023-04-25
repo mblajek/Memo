5258227be524c0aab00a61b49c1cd6f6b0ce9c1f
Ryszard <ryszard.poznanski0@gmail.com>
2023-04-25 20:20:37 +0200

    configure basic application

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,7 +1,5 @@
-import './bootstrap';
-
 (() => {
-    const e = document.getElementById("js-element");
+    const e = document.getElementById("js-element")!;
     e.innerHTML = "--js-test--";
     setInterval(() => e.innerHTML = e.innerHTML.slice(1) + e.innerHTML.slice(0, 1), 250);
 })();
