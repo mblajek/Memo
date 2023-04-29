ee93c5bbd2223ba2107cc5aae4f1382031884b9a
mblajek <mirkles@o2.pl>
2023-04-29 13:48:08 +0200

    FZ-19 added docs for translations endpint

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -4,17 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\App;
+use JsonException;
 use Locale;
+use OpenApi\Annotations\OpenApi as OA;
 
-class SystemController
+/** System endpoints without authorisation */
+class SystemController extends ApiController
 {
+    /**
+     * @OA\Get(
+     *     path="/api/v1/system/translation/{lang}/list",
+     *     tags={"System"},
+     *     summary = "All translations",
+     *     @OA\Parameter(name="lang", in="path", required=true, example="pl-pl", @OA\Schema(type="string")),
+     *     @OA\Response(response="200", description="Translations JSON")
+     * )
+     * @throws JsonException
+     */
     public function translationList(?string $locale): JsonResponse
     {
         $locale = Locale::canonicalize($locale);
         $locale = in_array($locale, config('app.available_locales'), strict: true) ? $locale : App::getLocale();
         $basePath = '../resources/lang';
         $index = 'index.json';
-        $readFile = fn(string $name): array => json_decode(file_get_contents("$basePath/$locale/$name"), true);
+        $readFile = fn(string $name): array => json_decode(
+            file_get_contents("$basePath/$locale/$name"),
+            associative: true,
+            flags: JSON_THROW_ON_ERROR
+        );
         $result = array_merge(["" => $locale], $readFile($index));
         foreach (scandir("$basePath/$locale") as $file) {
             if ($file !== $index && !str_starts_with($file, '.')) {
