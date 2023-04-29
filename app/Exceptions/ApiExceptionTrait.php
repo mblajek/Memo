63437ee6d272972f72a96bf328874e1ea67da8e3
mblajek <mirkles@o2.pl>
2023-04-29 21:02:29 +0200

    FZ-19 reorganise exception trait

--- app/Exceptions/ApiExceptionTrait.php
+++ app/Exceptions/ApiExceptionTrait.php
@@ -4,6 +4,11 @@ namespace App\Exceptions;
 
 trait ApiExceptionTrait
 {
+    public readonly string $errorCode;
+    public readonly ?array $errorData;
+
+    abstract public function __construct(string $errorCode, ?array $errorData = null);
+
     public function getJson(): string
     {
         return json_encode(array_filter(['code' => $this->errorCode, 'data' => $this->errorData]));
--- app/Exceptions/ApiFatalException.php
+++ app/Exceptions/ApiFatalException.php
@@ -8,10 +8,10 @@ class ApiFatalException extends RuntimeException
 {
     use ApiExceptionTrait;
 
-    public function __construct(
-        public readonly string $errorCode,
-        public readonly ?array $errorData = null
-    ) {
+    public function __construct(string $errorCode, ?array $errorData = null)
+    {
         parent::__construct($this->getJson());
+        $this->errorCode = $errorCode;
+        $this->errorData = $errorData;
     }
 }
