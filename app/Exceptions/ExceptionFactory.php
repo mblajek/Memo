9488206a821eaaacddf79101ae2665523c3ca112
mblajek <mirkles@o2.pl>
2023-05-04 02:52:02 +0200

    FZ-18 added some http exceptions

--- app/Exceptions/ExceptionFactory.php
+++ app/Exceptions/ExceptionFactory.php
@@ -8,4 +8,14 @@ class ExceptionFactory
     {
         return new ApiValidationException(400, 'exception.validation');
     }
+
+    public static function unauthorised(): ApiValidationException
+    {
+        return new ApiValidationException(401, 'exception.unauthorised');
+    }
+
+    public static function notFound(): ApiValidationException
+    {
+        return new ApiValidationException(404, 'exception.not_found');
+    }
 }
