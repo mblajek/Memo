9eb0a7229baed2ae831bd48ef27bf5537ad0e56c
Ryszard <ryszard.poznanski0@gmail.com>
2023-05-10 21:29:02 +0200

    create basic config and layout

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body class="antialiased">
+      <div id="root"></div>
+      @vite('resources/js/index.tsx')
+    </body>
+</html>
