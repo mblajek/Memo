2b11a262cb5669e83fbae57d1f619f6a7f28b703
mblajek <mirkles@o2.pl>
2023-05-23 21:43:25 +0200

    FZ-44 missing file, fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * @property string id
@@ -30,7 +29,6 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens;
     use HasFactory;
     use Notifiable;
     use UuidTrait;
