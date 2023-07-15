638bbbe3bf763ea07180a324272a76141a53ce38
mblajek <mirkles@o2.pl>
2023-07-15 13:26:09 +0200

    FZ-62 dictionary database and endpoint, ?in= filter

---
+++ app/Http/Resources/PositionResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Position;
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    schema: 'PositionResource',
+    properties: [
+        new OA\Property(property: 'id', type: 'string', format: 'uuid', example: 'UUID'),
+        new OA\Property(property: 'dictionaryId', type: 'string', format: 'uuid', example: 'UUID'),
+        new OA\Property(property: 'facilityId', type: 'string', format: 'uuid', example: 'UUID'),
+        new OA\Property(property: 'name', type: 'string', example: 'Test'),
+        new OA\Property(property: 'isFixed', type: 'bool', example: true),
+        new OA\Property(property: 'defaultOrder', type: 'int', example: 1),
+    ]
+)] /**
+ * @method __construct(Position $resource)
+ * @mixin Position
+ */
+class PositionResource extends AbstractJsonResource
+{
+    protected static function getMappedFields(): array
+    {
+        return ['id', 'dictionaryId', 'facilityId', 'name', 'isFixed', 'defaultOrder'];
+    }
+}
