c53c09bdc7ab7cb4e83b8132a2e19e5dc2f05b59
teba_kgolebiewski <k.golebiewski@teb-akademia.pl>
2023-08-10 19:44:24 +0200

    FZ-6 sending mail test
    Signed-off-by: teba_kgolebiewski <k.golebiewski@teb-akademia.pl>

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Permissions\Permission;
+use App\Http\Permissions\PermissionDescribe;
+use App\Mail\Test;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Mail;
+use OpenApi\Attributes as OA;
+
+class MailController extends ApiController
+{
+    protected function initPermissions(): void
+    {
+        $this->permissionOneOf(Permission::any);
+    }
+
+    #[OA\Post(
+        path: '/api/v1/mail/test',
+        description: new PermissionDescribe(Permission::any),
+        summary: 'Mail test',
+        tags: ['Mail'],
+        responses: [
+            new OA\Response(response: 200, description: 'OK'),
+            new OA\Response(response: 400, description: 'Bad Request'),
+            new OA\Response(response: 401, description: 'Unauthorised'),
+        ]
+    )]
+    public function test(): JsonResponse
+    {
+        Mail::to('kacperx95@gmail.com')->send(new Test());
+
+        return new JsonResponse();
+    }
+}
