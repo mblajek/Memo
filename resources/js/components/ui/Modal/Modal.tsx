9ef538678e4d3d833c046d550d3f26ad187ba8d6
TPReal <tpreal@gmail.com>
2023-08-18 12:54:19 +0200

    FZ-79 Improvements in modal styling. Modal width can be set to narrow. Modal gets vertical scrolling when not fitting on screen.

--- resources/js/components/ui/Modal/Modal.tsx
+++ resources/js/components/ui/Modal/Modal.tsx
@@ -1,11 +1,14 @@
 import * as dialog from "@zag-js/dialog";
 import {normalizeProps, useMachine} from "@zag-js/solid";
+import {cx} from "components/utils";
 import {ParentComponent, Show, createMemo, createRenderEffect, createUniqueId} from "solid-js";
 import {Portal} from "solid-js/web";
 import s from "./Modal.module.scss";
 
 interface Props {
   title?: string;
+  /** Width of the modal. If not specified, it adjusts to contents, with a reasonable minimum. */
+  width?: "narrow";
   open: boolean;
   /**
    * Event triggered when the Esc key is pressed or when the user clicks outside of the modal.
@@ -33,17 +36,20 @@ export const Modal: ParentComponent<Props> = (props) => {
       api().close();
     }
   });
+  const modalWidthClass = () => (props.width === "narrow" ? s.narrow : undefined);
   return (
     <Show when={api().isOpen}>
       <Portal>
-        <div class={s.modal}>
+        <div class={cx(s.modal, modalWidthClass())}>
           <div {...api().backdropProps} />
           <div {...api().containerProps}>
             <div {...api().contentProps}>
-              <Show when={props.title}>
-                <h2 {...api().titleProps}>{props.title}</h2>
-              </Show>
-              <div>{props.children}</div>
+              <div class={s.innerContent}>
+                <Show when={props.title}>
+                  <h2 {...api().titleProps}>{props.title}</h2>
+                </Show>
+                <div>{props.children}</div>
+              </div>
             </div>
           </div>
         </div>
