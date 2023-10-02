9c1910984b8bb08e5170245f41f6438eec4a7228
Greg Anielak <grzegorz.anielak@gmail.com>
2023-10-02 11:09:56 +0000

    Simplify TableSummary: summaryTranslation prop is unused.

--- resources/js/components/ui/Table/TableSummary.tsx
+++ resources/js/components/ui/Table/TableSummary.tsx
@@ -1,4 +1,4 @@
-import {LangEntryFunc, useLangFunc} from "components/utils";
+import {useLangFunc} from "components/utils";
 import {Show, VoidComponent} from "solid-js";
 import {tableStyle as ts, useTable} from ".";
 
@@ -8,11 +8,6 @@ interface Props {
    * table object.
    */
   rowsCount?: number;
-  /**
-   * Translation entry for the plural summary, taking the number of rows. Defaults to the value from
-   * meta.translations in table, and then to a generic default.
-   */
-  summaryTranslation?: LangEntryFunc;
 }
 
 export const TableSummary: VoidComponent<Props> = (props) => {
@@ -22,17 +17,10 @@ export const TableSummary: VoidComponent<Props> = (props) => {
   return (
     <div class={ts.tableSummary}>
       <Show
-        when={props.summaryTranslation}
-        fallback={
-          <Show
-            when={table.options.meta?.translations?.summary?.({count: count(), defaultValue: ""})}
-            fallback={t("tables.tables.generic.summary", {count: count()})}
-          >
-            {(summary) => <>{summary()}</>}
-          </Show>
-        }
+        when={table.options.meta?.translations?.summary?.({count: count(), defaultValue: ""})}
+        fallback={t("tables.tables.generic.summary", {count: count()})}
       >
-        {(summaryTranslation) => <>{summaryTranslation()({count: count()})}</>}
+        {(summary) => <>{summary()}</>}
       </Show>
     </div>
   );
