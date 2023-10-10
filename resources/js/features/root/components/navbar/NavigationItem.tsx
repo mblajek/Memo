a679b9dbc7cbb817f55985faf673a2426a4a94b2
TPReal <tpreal@gmail.com>
2023-10-10 08:51:18 +0200

    Invalidate all queries on clicking the currently active menu item.

--- resources/js/features/root/components/navbar/NavigationItem.tsx
+++ resources/js/features/root/components/navbar/NavigationItem.tsx
@@ -1,4 +1,6 @@
 import {A, AnchorProps} from "@solidjs/router";
+import {useQueryClient} from "@tanstack/solid-query";
+import {cx, htmlAttributes} from "components/utils";
 import {IconTypes} from "solid-icons";
 import {ParentComponent, splitProps} from "solid-js";
 import {Dynamic} from "solid-js/web";
@@ -7,13 +9,23 @@ export interface NavigationItemProps extends AnchorProps {
   icon: IconTypes;
 }
 
+/** Marker class to detect navigation item activity. */
+const ACTIVE_ITEM_CLASS = "__activeNavItem";
+
 export const NavigationItem: ParentComponent<NavigationItemProps> = (props) => {
-  const [local, rest] = splitProps(props, ["children", "icon"]);
+  const [local, aProps] = splitProps(props, ["children", "icon"]);
+  const queryClient = useQueryClient();
   return (
     <A
-      {...rest}
-      class="mb-2 py-2 px-4 rounded-lg flex flex-row items-center gap-3 no-underline text-black hover:bg-white"
-      activeClass="bg-white"
+      {...htmlAttributes.merge(aProps, {
+        class: "mb-2 py-2 px-4 rounded-lg flex flex-row items-center gap-3 no-underline text-black hover:bg-white",
+      })}
+      activeClass={cx("bg-white", ACTIVE_ITEM_CLASS)}
+      onClick={(event) => {
+        if (event.currentTarget.classList.contains(ACTIVE_ITEM_CLASS)) {
+          queryClient.invalidateQueries();
+        }
+      }}
     >
       <Dynamic component={local.icon} size="25" />
       <span>{local.children}</span>
