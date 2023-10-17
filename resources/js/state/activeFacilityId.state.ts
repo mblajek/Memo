4f1c3f15c3a732a4e71ece17bd5449c9d6d448d7
TPReal <tpreal@gmail.com>
2023-10-17 12:47:09 +0200

    Wrapped active facility in memo. (review)

--- resources/js/state/activeFacilityId.state.ts
+++ resources/js/state/activeFacilityId.state.ts
@@ -1,5 +1,5 @@
 import {createQuery} from "@tanstack/solid-query";
-import {Accessor, createSignal} from "solid-js";
+import {Accessor, createMemo, createSignal} from "solid-js";
 import {FacilityResource, System} from "../data-access/memo-api";
 
 /** The facility selected in the page header, or undefined when not set. */
@@ -7,6 +7,6 @@ export const [activeFacilityId, setActiveFacilityId] = createSignal<string>();
 
 export function useActiveFacility(): Accessor<FacilityResource | undefined> {
   const facilitiesQuery = createQuery(System.facilitiesQueryOptions);
-  // eslint-disable-next-line solid/reactivity
-  return () => (activeFacilityId() ? facilitiesQuery.data?.find(({id}) => id === activeFacilityId()) : undefined);
+  const activeFacility = createMemo(() => facilitiesQuery.data?.find(({id}) => id === activeFacilityId()));
+  return activeFacility;
 }
