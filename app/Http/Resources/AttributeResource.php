b876b48d9c1a3aa6c5af5a23aad626d3d866ca65
mblajek <mirkles@o2.pl>
2023-11-12 12:54:17 +0100

    help fixes

--- app/Http/Resources/AttributeResource.php
+++ app/Http/Resources/AttributeResource.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Str;
         new OA\Property(property: 'model', type: 'string', enum: AttributeModel::class, example: 'user'),
         new OA\Property(property: 'name', type: 'string', example: 'attribute'),
         new OA\Property(property: 'apiName', type: 'string', example: 'attribute'),
-        new OA\Property(property: 'type', type: 'string', enum: AttributeType::class, example: 'decimal0'),
+        new OA\Property(property: 'type', type: 'string', enum: AttributeType::class, example: 'int'),
         new OA\Property(property: 'dictionaryId', type: 'string', format: 'uuid', example: 'UUID', nullable: true),
         new OA\Property(property: 'defaultOrder', type: 'int', example: 1),
         new OA\Property(property: 'isMultiValue', type: 'bool', example: false, nullable: true),
