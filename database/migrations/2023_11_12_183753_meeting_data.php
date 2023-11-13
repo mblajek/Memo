cac305f45d4e3e27183e44d94b13cad12b5d5408
Damian Sepielak <damian.sepielak@gmail.com>
2023-11-13 16:28:36 +0100

    FZ-48 [B] Code review 2: changes in some column names and types, documentation update.
    Additionally changed to consistent camelCase for properties.

--- database/migrations/2023_11_12_183753_meeting_data.php
+++ database/migrations/2023_11_12_183753_meeting_data.php
@@ -11,11 +11,13 @@ return new class extends Migration
     private string $meetingCategoryDictionaryId = 'ce12aa23-a5db-49f3-987b-d2db3ab24a3b';
     private string $meetingTypeDictionaryId = '4cc5d6b5-5913-4e07-9fcb-09b6f3d3d2a0';
     private string $meetingStatusDictionaryId = '3865a3c3-0038-4668-9d55-5d05b79d7fcd';
+    private string $attendanceStatusDictionaryId = 'a2874757-aca7-4c16-a0dc-2fc368f795fb';
     private string $meetingResourceDictionaryId = 'fdb0f818-aa1e-4ed9-97cc-2a3cb1b702df';
-    private string $meeting_category_attribute_id = '8111626d-130c-454d-b0c0-9fda9ab9917a';
-    private string $meeting_type_attribute_id = '5f7d5e66-03f9-4bcd-a726-fde82cf98d6f';
-    private string $meeting_status_attribute_id = 'e443e2c2-82fc-41d3-8fda-fe374e5329d3';
-    private string $meeting_resource_attribute_id = 'ff2fb31e-c992-4e2d-b543-f2768ee1c897';
+    private string $meetingCategoryAttributeId = '8111626d-130c-454d-b0c0-9fda9ab9917a';
+    private string $meetingTypeAttributeId = '5f7d5e66-03f9-4bcd-a726-fde82cf98d6f';
+    private string $meetingStatusAttributeId = 'e443e2c2-82fc-41d3-8fda-fe374e5329d3';
+    private string $attendanceStatusAttributeId = 'c005ade5-2d01-4576-b050-93c9e7251518';
+    private string $meetingResourceAttributeId = 'ff2fb31e-c992-4e2d-b543-f2768ee1c897';
 
     /**
      * Run the migrations.
@@ -30,7 +32,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->text('notes')->nullable();
             $table->date('date');
-            $table->integer('start_day_minute');
+            $table->integer('start_dayminute');
             $table->integer('duration_minutes');
             $table->char('status_dict_id', 36)->collation('ascii_bin');
             $table->char('created_by', 36)->collation('ascii_bin');
@@ -68,7 +70,7 @@ return new class extends Migration
             $table->char('meeting_id', 36)->collation('ascii_bin');
             $table->char('user_id', 36)->collation('ascii_bin');
             $table->string('attendance_type', 36)->collation('ascii_bin');
-            $table->boolean('was_present')->nullable();
+            $table->char('attendance_status_dict_id', 36)->collation('ascii_bin')->nullable();
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
 
@@ -80,6 +82,11 @@ return new class extends Migration
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users')
+                ->restrictOnDelete();
+
+            $table->foreign('attendance_status_dict_id')
+                ->references('id')
+                ->on('positions')
                 ->restrictOnDelete();
         });
 
@@ -123,11 +130,20 @@ return new class extends Migration
                 'created_at' => $timestamp,
                 'updated_at' => $timestamp,
             ],
+            [
+                'id' => $this->attendanceStatusDictionaryId,
+                'name' => 'attendanceStatus',
+                'is_fixed' => true,
+                'is_extendable' => true,
+                'created_by' => $systemUserId,
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ],
         ], 'id');
 
         DB::table('attributes')->upsert([
             [
-                'id' => $this->meeting_category_attribute_id,
+                'id' => $this->meetingCategoryAttributeId,
                 'facility_id' => null,
                 'table' => 'meetings',
                 'model' => 'meeting',
@@ -142,7 +158,7 @@ return new class extends Migration
                 'updated_at' => $timestamp,
             ],
             [
-                'id' => $this->meeting_type_attribute_id,
+                'id' => $this->meetingTypeAttributeId,
                 'facility_id' => null,
                 'table' => 'meetings',
                 'model' => 'meeting',
@@ -157,7 +173,7 @@ return new class extends Migration
                 'updated_at' => $timestamp,
             ],
             [
-                'id' => $this->meeting_status_attribute_id,
+                'id' => $this->meetingStatusAttributeId,
                 'facility_id' => null,
                 'table' => 'meetings',
                 'model' => 'meeting',
@@ -172,7 +188,22 @@ return new class extends Migration
                 'updated_at' => $timestamp,
             ],
             [
-                'id' => $this->meeting_resource_attribute_id,
+                'id' => $this->attendanceStatusAttributeId,
+                'facility_id' => null,
+                'table' => 'meeting_attendants',
+                'model' => 'meeting_attendant',
+                'name' => 'attendance_status',
+                'api_name' => 'attendance_status_dict_id',
+                'type' => 'dict',
+                'dictionary_id' => $this->attendanceStatusDictionaryId,
+                'default_order' => 1,
+                'is_multi_value' => null,
+                'requirement_level' => 'optional',
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ],
+            [
+                'id' => $this->meetingResourceAttributeId,
                 'facility_id' => null,
                 'table' => 'meeting_resources',
                 'model' => 'meeting_resource',
@@ -195,14 +226,17 @@ return new class extends Migration
      */
     public function down(): void
     {
+        DB::table('positions')->where('dictionary_id', $this->attendanceStatusDictionaryId)->delete();
         DB::table('positions')->where('dictionary_id', $this->meetingStatusDictionaryId)->delete();
         DB::table('positions')->where('dictionary_id', $this->meetingResourceDictionaryId)->delete();
 
-        DB::table('attributes')->delete($this->meeting_category_attribute_id);
-        DB::table('attributes')->delete($this->meeting_type_attribute_id);
-        DB::table('attributes')->delete($this->meeting_status_attribute_id);
-        DB::table('attributes')->delete($this->meeting_resource_attribute_id);
+        DB::table('attributes')->delete($this->attendanceStatusAttributeId);
+        DB::table('attributes')->delete($this->meetingCategoryAttributeId);
+        DB::table('attributes')->delete($this->meetingTypeAttributeId);
+        DB::table('attributes')->delete($this->meetingStatusAttributeId);
+        DB::table('attributes')->delete($this->meetingResourceAttributeId);
 
+        DB::table('dictionaries')->delete($this->attendanceStatusDictionaryId);
         DB::table('dictionaries')->delete($this->meetingStatusDictionaryId);
         DB::table('dictionaries')->delete($this->meetingResourceDictionaryId);
 
