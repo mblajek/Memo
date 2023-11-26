16ca3ae4d84f8cda96de68d8e33e2bd4e23b75d9
Damian Sepielak <damian.sepielak@gmail.com>
2023-11-26 14:33:30 +0100

    FZ-94 Meeting related models

---
+++ app/Models/Meeting.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\QueryBuilders\MeetingBuilder;
+use App\Utils\Uuid\UuidTrait;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * @property string id
+ * @property string facility_id
+ * @property string category_dict_id
+ * @property string type_dict_id
+ * @property string name
+ * @property string notes
+ * @property int start_dayminute
+ * @property int duration_minutes
+ * @property string status_dict_id
+ * @property string created_by
+ * @property CarbonImmutable created_at
+ * @property CarbonImmutable updated_at
+ * @property-read Collection|MeetingAttendant[] $attendants
+ * @property-read Collection|MeetingResource[] $resources
+ * @method static MeetingBuilder query()
+ */
+class Meeting extends Model
+{
+    use UuidTrait;
+
+    protected $table = 'meetings';
+
+    protected $fillable = [
+        'id',
+        'facility_id',
+        'category_dict_id',
+        'type_dict_id',
+        'name',
+        'notes',
+        'start_dayminute',
+        'duration_minutes',
+        'status_dict_id',
+        'created_by',
+    ];
+
+    protected $casts = [
+        'created_at' => 'immutable_datetime',
+        'updated_at' => 'immutable_datetime',
+    ];
+
+    public function attendants(): HasMany
+    {
+        return $this->hasMany(MeetingAttendant::class);
+    }
+
+    public function resources(): HasMany
+    {
+        return $this->hasMany(MeetingResource::class);
+    }
+}
---
+++ app/Models/MeetingResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\QueryBuilders\MeetingResourceBuilder;
+use App\Utils\Uuid\UuidTrait;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string id
+ * @property string meeting_id
+ * @property string resource_dict_id
+ * @property CarbonImmutable created_at
+ * @property CarbonImmutable updated_at
+ * @method static MeetingResourceBuilder query()
+ */
+class MeetingResource extends Model
+{
+    use UuidTrait;
+
+    protected $table = 'meeting_resources';
+
+    protected $fillable = [
+        'id',
+        'meeting_id',
+        'resource_dict_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'immutable_datetime',
+        'updated_at' => 'immutable_datetime',
+    ];
+}
