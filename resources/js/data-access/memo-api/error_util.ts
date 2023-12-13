a6d418547bdea1128e252fe3c2313e8253942fbf
TPReal <tpreal@gmail.com>
2023-12-13 18:55:02 +0100

    Minor improvement in error message processing. Minor style fix.

--- resources/js/data-access/memo-api/error_util.ts
+++ resources/js/data-access/memo-api/error_util.ts
@@ -3,7 +3,7 @@ import {Api} from "./types";
 
 export function translateError(error: Api.Error, t = useLangFunc()) {
   return t(error.code, {
-    ...(Api.isValidationError(error) ? {attribute: error.field} : undefined),
+    ...(Api.isValidationError(error) ? {attribute: t("validation.quoted_field_name", {text: error.field})} : undefined),
     ...error.data,
   });
 }
