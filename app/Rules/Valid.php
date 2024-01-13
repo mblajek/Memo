2f11b5dcbfd1865984ad20be96379004184da571
Dominik Oledzki <dominik.oledzki@gmail.com>
2024-01-13 21:20:52 +0100

    Fix unit test

--- app/Rules/Valid.php
+++ app/Rules/Valid.php
@@ -164,7 +164,7 @@ class Valid extends AbstractDataRule
         return array_merge(
             $sometimes ? ['sometimes'] : [],
             // todo: consider: ['present', new self($nullable, ['bail', ...$rules]), ...$additionalRules],
-            ['present', new self($nullable, array_merge(['bail'], $rules, $additionalRules))],
+            [new self($nullable, array_merge(['bail'], $rules, $additionalRules))],
         );
     }
 }
