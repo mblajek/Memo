17613c603461461f692cc1e14d680ce91cdf81d8
TPReal <tpreal@gmail.com>
2024-01-15 20:38:13 +0100

    Fixed the buttons contrast to meet the WCAG requirements.

--- resources/js/features/root/layout/Footer.tsx
+++ resources/js/features/root/layout/Footer.tsx
@@ -4,8 +4,8 @@ import s from "./layout.module.scss";
 export const Footer: VoidComponent = () => {
   return (
     <footer class={s.footer}>
-      <div id="copyright">Copyright ©2023 nie mam pojęcia jak działa copyright</div>
-      <div id="version">Memo Version: 0.1 Alpha</div>
+      <div id="copyright">Copyright ©2024 nie mam pojęcia jak działa copyright</div>
+      <div id="version">Memo Version: 0.2 Alpha</div>
     </footer>
   );
 };
