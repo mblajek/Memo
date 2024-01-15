14284cca4d95a4ede87486680bfd68232b249289
TPReal <tpreal@gmail.com>
2024-01-15 23:25:11 +0100

    Added base info and meeting lists on the staff and client details pages.

--- resources/js/features/root/pages/ClientDetails.page.tsx
+++ resources/js/features/root/pages/ClientDetails.page.tsx
@@ -1,7 +1,36 @@
 import {useParams} from "@solidjs/router";
-import {VoidComponent} from "solid-js";
+import {createQuery} from "@tanstack/solid-query";
+import {BigSpinner} from "components/ui/Spinner";
+import {QueryBarrier} from "components/utils";
+import {FacilityClient} from "data-access/memo-api/groups/FacilityClient";
+import {UserDetailsHeader} from "features/facility-users/UserDetailsHeader";
+import {UserMeetingsTables} from "features/facility-users/UserMeetingsTables";
+import {Show, VoidComponent} from "solid-js";
 
 export default (() => {
   const params = useParams();
-  return <>client {params.userId}</>;
+  const userId = () => params.userId!;
+  const dataQuery = createQuery(() => FacilityClient.clientQueryOptions(userId()));
+  return (
+    <div class="m-2">
+      <QueryBarrier queries={[dataQuery]} ignoreCachedData>
+        <Show when={dataQuery.data} fallback={<BigSpinner />}>
+          {(user) => (
+            <div class="flex flex-col items-stretch gap-2">
+              <UserDetailsHeader type="clients" user={user()} />
+              <UserMeetingsTables
+                intrinsicFilter={{
+                  type: "column",
+                  column: "clients.*.userId",
+                  op: "has",
+                  val: userId(),
+                }}
+                staticPersistenceKey="clientMeetings"
+              />
+            </div>
+          )}
+        </Show>
+      </QueryBarrier>
+    </div>
+  );
 }) satisfies VoidComponent;
--- resources/js/features/root/pages/MeetingsList.page.tsx
+++ resources/js/features/root/pages/MeetingsList.page.tsx
@@ -1,56 +1,39 @@
-import {PaddedCell, createTableTranslations} from "components/ui/Table";
+import {createTableTranslations} from "components/ui/Table";
 import {TQueryTable} from "components/ui/Table/TQueryTable";
-import {EN_DASH} from "components/ui/symbols";
-import {formatDayMinuteHM} from "components/utils/day_minute_util";
 import {FacilityMeeting} from "data-access/memo-api/groups/FacilityMeeting";
+import {useMeetingTableColumns} from "features/meeting/meeting_tables";
 import {VoidComponent} from "solid-js";
 import {activeFacilityId} from "state/activeFacilityId.state";
 
 export default (() => {
+  const meetingTableColumns = useMeetingTableColumns();
   return (
-    <>
-      <TQueryTable
-        mode="standalone"
-        staticPrefixQueryKey={FacilityMeeting.keys.meeting()}
-        staticEntityURL={`facility/${activeFacilityId()}/meeting`}
-        staticTranslations={createTableTranslations("meeting")}
-        staticPersistenceKey="facilityMeetings"
-        intrinsicSort={[{type: "column", column: "date", desc: true}]}
-        columns={[
-          {name: "id", initialVisible: false},
-          {name: "date"},
-          {
-            name: "startDayminute",
-            extraDataColumns: ["durationMinutes"],
-            columnDef: {
-              cell: (c) => (
-                <PaddedCell>
-                  {formatDayMinuteHM(c.row.getValue<number>("startDayminute"), {hour: "2-digit"})} {EN_DASH}{" "}
-                  {formatDayMinuteHM(
-                    c.row.getValue<number>("startDayminute") + c.row.getValue<number>("durationMinutes"),
-                    {hour: "2-digit"},
-                  )}
-                </PaddedCell>
-              ),
-              enableColumnFilter: false,
-              size: 120,
-            },
-          },
-          {name: "durationMinutes", initialVisible: false, columnDef: {size: 120}},
-          {name: "categoryDictId", initialVisible: false},
-          {name: "typeDictId"},
-          {name: "statusDictId"},
-          {name: "staff.*.name"},
-          {name: "clients.*.name"},
-          {name: "isRemote"},
-          {name: "notes"},
-          {name: "resources.*.dictId"},
-          {name: "createdAt", columnDef: {sortDescFirst: true}, initialVisible: false},
-          {name: "createdBy.name", initialVisible: false},
-          {name: "updatedAt", columnDef: {sortDescFirst: true}, initialVisible: false},
-        ]}
-        initialSort={[{id: "date", desc: true}]}
-      />
-    </>
+    <TQueryTable
+      mode="standalone"
+      staticPrefixQueryKey={FacilityMeeting.keys.meeting()}
+      staticEntityURL={`facility/${activeFacilityId()}/meeting`}
+      staticTranslations={createTableTranslations("meeting")}
+      staticPersistenceKey="facilityMeetings"
+      intrinsicSort={[{type: "column", column: "date", desc: true}]}
+      columns={meetingTableColumns.get(
+        "id",
+        "date",
+        "time",
+        "duration",
+        "category",
+        "type",
+        "status",
+        "staff",
+        "clients",
+        "isRemote",
+        "notes",
+        "resources",
+        "createdAt",
+        "createdBy",
+        "updatedAt",
+        "actions",
+      )}
+      initialSort={[{id: "date", desc: true}]}
+    />
   );
 }) satisfies VoidComponent;
