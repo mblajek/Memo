49794ca2903d12ff4ac80d947dddee624741a2e6
mblajek <mirkles@o2.pl>
2024-01-24 23:28:08 +0100

    FZ-129 attendant.name type

--- app/Tquery/Tables/MeetingAttendantTquery.php
+++ app/Tquery/Tables/MeetingAttendantTquery.php
@@ -43,7 +43,7 @@ readonly class MeetingAttendantTquery extends MeetingTquery
             'attendant.user_id'
         );
         $config->addJoined(
-            TqDataTypeEnum::uuid,
+            TqDataTypeEnum::string,
             TqTableAliasEnum::attendant,
             'name',
             'attendant.name'
