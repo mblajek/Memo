6659bbb1410be9483f77264be29b5235ce99518d
TPReal <tpreal@gmail.com>
2024-01-30 12:14:12 +0100

    Fix/simplification in the cache.

--- resources/js/components/utils/cache.ts
+++ resources/js/components/utils/cache.ts
@@ -1,19 +1,18 @@
 import {Accessor} from "solid-js";
 
-export function createCached<T, Args extends unknown[], R>(
-  creator: Accessor<T>,
-  transform: (t: T, ...args: Args) => R,
-): Accessor<R>;
+export function createCached<T, R>(creator: Accessor<T>, transform: (t: T) => R): Accessor<R>;
+/**
+ * Creates a cached state. A typical use case is caching a memo which depends on a query. Without caching,
+ * the query would be subscribed to multiple times, possibly hundreds of times if used in a small component,
+ * and TanStack Query cache performance will affect the page performance.
+ */
 export function createCached<T>(creator: Accessor<T>): Accessor<T>;
-export function createCached<T, Args extends unknown[], R>(
-  creator: Accessor<T>,
-  transform = (t: T, ..._args: Args) => t as unknown as R,
-) {
+export function createCached<T, R>(creator: Accessor<T>, transform = (t: T) => t as unknown as R) {
   let data: T | undefined;
-  return (...args: Args) => {
-    if (!data) {
+  return () => {
+    if (data === undefined) {
       data = creator();
     }
-    return transform(data, ...args);
+    return transform(data);
   };
 }
