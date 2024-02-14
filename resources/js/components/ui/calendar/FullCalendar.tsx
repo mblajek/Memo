c409124e19a31466ee31830eee5c3c72a230f5ee
TPReal <tpreal@gmail.com>
2024-02-14 19:00:17 +0100

    Switched creating meeting series to the new "clone" endpoint.

--- resources/js/components/ui/calendar/FullCalendar.tsx
+++ resources/js/components/ui/calendar/FullCalendar.tsx
@@ -51,7 +51,7 @@ import {HoursArea} from "./calendar-columns/HoursArea";
 import {ResourceHeader} from "./calendar-columns/ResourceHeader";
 import {HoursAreaBlock} from "./calendar-columns/blocks";
 import {MeetingEventBlock} from "./calendar-columns/events";
-import {getRandomEventColors} from "./colors";
+import {coloringToStyle, getRandomEventColors} from "./colors";
 import {DaysRange} from "./days_range";
 import {Block, PartDayTimeSpan} from "./types";
 import {WeekDaysCalculator} from "./week_days_calculator";
@@ -147,7 +147,7 @@ export const FullCalendar: VoidComponent<Props> = (propsArg) => {
                 style={{
                   width: "14px",
                   height: "14px",
-                  ...coloring.regular,
+                  ...coloringToStyle(coloring),
                 }}
               />
             </div>
