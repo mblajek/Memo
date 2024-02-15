265acc2dee158b03d798050481f71f3dbd6d9956
TPReal <tpreal@gmail.com>
2024-02-15 19:08:26 +0100

    Block editing system users. For now system users are identified as having emails without @, this should be fixed once backend supports it.

--- resources/js/features/root/pages/AdminUsersList.page.tsx
+++ resources/js/features/root/pages/AdminUsersList.page.tsx
@@ -3,11 +3,12 @@ import {Email} from "components/ui/Email";
 import {AUTO_SIZE_COLUMN_DEFS, PaddedCell, cellFunc, createTableTranslations} from "components/ui/Table";
 import {TQueryTable} from "components/ui/Table/TQueryTable";
 import {USER_ICONS} from "components/ui/icons";
+import {EMPTY_VALUE_SYMBOL} from "components/ui/symbols";
 import {useLangFunc} from "components/utils";
 import {Admin} from "data-access/memo-api/groups";
 import {createUserCreateModal} from "features/user-edit/user_create_modal";
 import {createUserEditModal} from "features/user-edit/user_edit_modal";
-import {VoidComponent} from "solid-js";
+import {Show, VoidComponent} from "solid-js";
 
 export default (() => {
   const t = useLangFunc();
@@ -47,11 +48,20 @@ export default (() => {
         {
           name: "actions",
           isDataColumn: false,
-          extraDataColumns: ["id"],
+          // TODO: Introduce a proper safety check for unmodifiable users once the backend supports it.
+          extraDataColumns: ["id", "email"],
           columnDef: {
             cell: (c) => (
               <PaddedCell>
-                <EditButton class="minimal" onClick={() => userEditModal.show({userId: c.row.original.id as string})} />
+                <Show
+                  when={!c.row.original.email || (c.row.original.email as string).includes("@")}
+                  fallback={EMPTY_VALUE_SYMBOL}
+                >
+                  <EditButton
+                    class="minimal"
+                    onClick={() => userEditModal.show({userId: c.row.original.id as string})}
+                  />
+                </Show>
               </PaddedCell>
             ),
             enableSorting: false,
