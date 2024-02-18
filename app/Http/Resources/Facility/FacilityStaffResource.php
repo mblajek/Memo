bc270c40e33cabeb0abb58e1c2e09d6508ca2020
mblajek <mirkles@o2.pl>
2024-02-18 20:16:53 +0100

    FZ-126 add missing fields to list clients and staff

--- app/Http/Resources/Facility/FacilityStaffResource.php
+++ app/Http/Resources/Facility/FacilityStaffResource.php
@@ -9,15 +9,19 @@ use OpenApi\Attributes as OA;
 #[OA\Schema(
     schema: 'FacilityStaffResource',
     properties: [
+        new OA\Property(property: 'hasFacilityAdmin', type: 'bool', example: false),
     ]
 )] /**
  * @method __construct(User $resource)
+ * @property bool has_facility_admin
  * @mixin User
  */
 class FacilityStaffResource extends AbstractJsonResource
 {
     protected static function getMappedFields(): array
     {
-        return [];
+        return [
+            'hasFacilityAdmin' => true,
+        ];
     }
 }
