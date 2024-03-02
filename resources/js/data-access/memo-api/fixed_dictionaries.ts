4782649e63a169bb536ba5ee806072f8faaacfab
TPReal <tpreal@gmail.com>
2024-03-02 13:47:42 +0100

    Change of the attendanceType field to a dictionary.

--- resources/js/data-access/memo-api/fixed_dictionaries.ts
+++ resources/js/data-access/memo-api/fixed_dictionaries.ts
@@ -1,46 +1,35 @@
 import {Accessor, createMemo} from "solid-js";
 import {Dictionaries, TypedDictionary, typedDictionary, useDictionaries} from "./dictionaries";
 
-export function useMeetingTypeDictionary(dictionaries?: DictionariesInput) {
-  return useTypedDictionary({
-    dictionaries,
-    dictionaryName: "meetingType",
-    positionNames: ["work_time", "leave_time", "other"],
-  });
-}
-
-export function useMeetingCategoryDictionary(dictionaries?: DictionariesInput) {
-  return useTypedDictionary({
-    dictionaries,
-    dictionaryName: "meetingCategory",
-    positionNames: ["other", "system"],
-  });
-}
-
-export function useMeetingStatusDictionary(dictionaries?: DictionariesInput) {
-  return useTypedDictionary({
-    dictionaries,
-    dictionaryName: "meetingStatus",
-    positionNames: ["planned", "completed", "cancelled"],
-  });
-}
-
-export function useAttendanceStatusDictionary(dictionaries?: DictionariesInput) {
-  return useTypedDictionary({
-    dictionaries,
-    dictionaryName: "attendanceStatus",
-    positionNames: ["ok", "cancelled", "no_show", "late_present", "too_late"],
-  });
-}
-
 export function useFixedDictionaries(dictionaries?: DictionariesInput) {
   const dicts = unwrap(dictionaries);
   return {
     dictionaries: dicts,
-    meetingCategoryDict: useMeetingCategoryDictionary(dicts),
-    meetingTypeDict: useMeetingTypeDictionary(dicts),
-    meetingStatusDict: useMeetingStatusDictionary(dicts),
-    attendanceStatusDict: useAttendanceStatusDictionary(dicts),
+    meetingCategoryDict: useTypedDictionary({
+      dictionaries,
+      dictionaryName: "meetingCategory",
+      positionNames: ["other", "system"],
+    }),
+    meetingTypeDict: useTypedDictionary({
+      dictionaries,
+      dictionaryName: "meetingType",
+      positionNames: ["work_time", "leave_time", "other"],
+    }),
+    meetingStatusDict: useTypedDictionary({
+      dictionaries,
+      dictionaryName: "meetingStatus",
+      positionNames: ["planned", "completed", "cancelled"],
+    }),
+    attendanceTypeDict: useTypedDictionary({
+      dictionaries,
+      dictionaryName: "attendanceType",
+      positionNames: ["staff", "client"],
+    }),
+    attendanceStatusDict: useTypedDictionary({
+      dictionaries,
+      dictionaryName: "attendanceStatus",
+      positionNames: ["ok", "cancelled", "no_show", "late_present", "too_late"],
+    }),
   };
 }
 
--- resources/js/features/root/pages/MeetingAttendantsList.page.tsx
+++ resources/js/features/root/pages/MeetingAttendantsList.page.tsx
@@ -30,6 +30,7 @@ export default (() => {
         "time",
         "duration",
         "isInSeries",
+        "seriesType",
         "category",
         "type",
         "statusTags",
