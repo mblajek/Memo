046ca1192a3bdd3be5d29bb468e6a2d2a1e4df02
TPReal <tpreal@gmail.com>
2024-05-26 23:30:57 +0200

    Moved the docs branch name from code to the env file.

--- resources/js/features/root/pages/help/Help.page.tsx
+++ resources/js/features/root/pages/help/Help.page.tsx
@@ -6,16 +6,14 @@ import {currentLanguage} from "i18n_loader";
 import {VoidComponent} from "solid-js";
 import {Help} from "./Help";
 
-const MASTER_DOCS_BRANCH = "master-docs";
-
 export default (() => {
   const t = useLangFunc();
   const params = useParams();
   return (
     <Help
       title={capitalizeString(t("routes.help"))}
-      // In DEV use the local docs files, otherwise use files directly from GitHub.
-      mdPath={`/docs/${isDEV() ? "" : `${MASTER_DOCS_BRANCH}/`}${currentLanguage()}/${params.helpPath}.md`}
+      // In DEV use the local docs files, otherwise use files hosted remotely.
+      mdPath={`/${isDEV() ? "docs" : "docs-remote"}/${currentLanguage()}/${params.helpPath}.md`}
     />
   );
 }) satisfies VoidComponent;
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/docs/{branch}/{path}', function (string $branch, string $path) {
-    $data = Cache::remember("docs/$branch/$path", 150 /* 2.5m */, function () use ($branch, $path): int|array {
-        $response = Http::get(str_replace('{branch}', $branch, env('DOCS_URL') . $path));
+Route::get('/docs-remote/{path}', function (string $path) {
+    $data = Cache::remember("docs-remote/$path", 150 /* 2.5m */, function () use ($path): int|array {
+        $response = Http::get(env('DOCS_URL') . $path);
         return ($response->status() === 200) ? [$response->header('Content-Type'), $response->body()] : 404;
     });
     return ($data === 404) ? ExceptionFactory::notFound()->render() :
