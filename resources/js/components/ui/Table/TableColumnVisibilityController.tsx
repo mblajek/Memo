ade851b7533eba3fdf4aafaf27a425a3523e2bfe
TPReal <tpreal@gmail.com>
2024-07-12 16:23:41 +0200

    Simple debouncing for table columns visibility.

--- resources/js/components/ui/Table/TableColumnVisibilityController.tsx
+++ resources/js/components/ui/Table/TableColumnVisibilityController.tsx
@@ -1,5 +1,6 @@
-import {cx, useLangFunc} from "components/utils";
-import {For, Show, VoidComponent, createSignal} from "solid-js";
+import {VisibilityState} from "@tanstack/solid-table";
+import {cx, debouncedAccessor, useLangFunc} from "components/utils";
+import {For, Show, VoidComponent, createComputed, createSignal} from "solid-js";
 import {ColumnName, useTable} from ".";
 import {Button} from "../Button";
 import {PopOver} from "../PopOver";
@@ -12,12 +13,14 @@ export const TableColumnVisibilityController: VoidComponent = () => {
   const table = useTable();
   const defaultColumnVisibility = table.options.meta?.defaultColumnVisibility;
   const columnGroupingInfo = table.options.meta?.tquery?.columnGroupingInfo;
+  const [visibility, setVisibility] = createSignal<Readonly<VisibilityState>>({});
   const isDefaultVisibility = () =>
-    table
-      .getAllLeafColumns()
-      .every(
-        (c) => columnGroupingInfo?.(c.id).isCount || c.getIsVisible() === (defaultColumnVisibility?.()[c.id] ?? true),
-      );
+    Object.entries(visibility()).every(
+      ([id, visible]) => columnGroupingInfo?.(id).isCount || visible === (defaultColumnVisibility?.()[id] ?? true),
+    );
+  // eslint-disable-next-line solid/reactivity
+  const debouncedVisibility = debouncedAccessor(visibility, {outputImmediately: () => isDefaultVisibility()});
+  createComputed(() => table.setColumnVisibility(debouncedVisibility()));
   const [resetHovered, setResetHovered] = createSignal(false);
   return (
     <PopOver
@@ -31,64 +34,79 @@ export const TableColumnVisibilityController: VoidComponent = () => {
         </Button>
       )}
     >
-      <div class="overflow-y-auto flex flex-col gap-0.5">
-        <div class="flex flex-col">
-          <For each={table.getAllLeafColumns()}>
-            {(column) => {
-              const groupingInfo = () => columnGroupingInfo?.(column.id);
-              return (
-                <Show when={groupingInfo() && !groupingInfo()?.isCount}>
-                  <label
-                    class={cx("px-2 pt-0.5 hover:bg-hover flex gap-1 items-baseline select-none", {
-                      "!bg-select": resetHovered() ? defaultColumnVisibility?.()[column.id] : column.getIsVisible(),
-                    })}
+      {() => {
+        const currentVisibility: VisibilityState = {};
+        for (const column of table.getAllLeafColumns()) {
+          currentVisibility[column.id] = column.getIsVisible();
+        }
+        setVisibility(currentVisibility);
+        return (
+          <>
+            <div class="overflow-y-auto flex flex-col gap-0.5">
+              <div class="flex flex-col">
+                <For each={table.getAllLeafColumns()}>
+                  {(column) => {
+                    const groupingInfo = () => columnGroupingInfo?.(column.id);
+                    return (
+                      <Show when={groupingInfo() && !groupingInfo()?.isCount}>
+                        <label
+                          class={cx("px-2 pt-0.5 hover:bg-hover flex gap-1 items-baseline select-none", {
+                            "!bg-select": resetHovered()
+                              ? defaultColumnVisibility?.()[column.id]
+                              : visibility()[column.id],
+                          })}
+                        >
+                          <input
+                            class={column.getCanHide() ? undefined : "invisible"}
+                            name={`column_visibility_${column.id}`}
+                            checked={visibility()[column.id]}
+                            onChange={({target}) => setVisibility((v) => ({...v, [column.id]: target.checked}))}
+                            type="checkbox"
+                            disabled={!column.getCanHide() || groupingInfo()?.isForceShown}
+                            use:title={
+                              groupingInfo()?.isForceShown
+                                ? t("tables.column_groups.column_status.force_shown")
+                                : undefined
+                            }
+                          />{" "}
+                          <ColumnName def={column.columnDef} />
+                          <Show when={groupingInfo()?.isGrouped}>
+                            <span class="text-memo-active" use:title={t("tables.column_groups.column_status.grouped")}>
+                              {t("tables.column_groups.grouping_symbol")}
+                            </span>
+                          </Show>
+                        </label>
+                      </Show>
+                    );
+                  }}
+                </For>
+              </div>
+            </div>
+            <div class="p-1 flex flex-col gap-1">
+              <Show when={defaultColumnVisibility}>
+                {(defaultColumnVisibility) => (
+                  <Button
+                    class="secondary small"
+                    onClick={() => setVisibility(defaultColumnVisibility())}
+                    disabled={isDefaultVisibility()}
+                    onMouseOver={[setResetHovered, true]}
+                    onMouseOut={[setResetHovered, false]}
                   >
-                    <input
-                      class={column.getCanHide() ? undefined : "invisible"}
-                      name={`column_visibility_${column.id}`}
-                      checked={column.getIsVisible()}
-                      onChange={column.getToggleVisibilityHandler()}
-                      type="checkbox"
-                      disabled={!column.getCanHide() || groupingInfo()?.isForceShown}
-                      use:title={
-                        groupingInfo()?.isForceShown ? t("tables.column_groups.column_status.force_shown") : undefined
-                      }
-                    />{" "}
-                    <ColumnName def={column.columnDef} />
-                    <Show when={groupingInfo()?.isGrouped}>
-                      <span class="text-memo-active" use:title={t("tables.column_groups.column_status.grouped")}>
-                        {t("tables.column_groups.grouping_symbol")}
-                      </span>
-                    </Show>
-                  </label>
-                </Show>
-              );
-            }}
-          </For>
-        </div>
-      </div>
-      <div class="p-1 flex flex-col gap-1">
-        <Show when={defaultColumnVisibility}>
-          {(defaultColumnVisibility) => (
-            <Button
-              class="secondary small"
-              onClick={() => table.setColumnVisibility(defaultColumnVisibility())}
-              disabled={isDefaultVisibility()}
-              onMouseOver={[setResetHovered, true]}
-              onMouseOut={[setResetHovered, false]}
-            >
-              {t("actions.restore_default")}
-            </Button>
-          )}
-        </Show>
-        <Button
-          class="secondary small"
-          onClick={() => table.setColumnSizing({})}
-          disabled={!Object.keys(table.getState().columnSizing).length}
-        >
-          {t("tables.reset_column_sizes")}
-        </Button>
-      </div>
+                    {t("actions.restore_default")}
+                  </Button>
+                )}
+              </Show>
+              <Button
+                class="secondary small"
+                onClick={() => table.setColumnSizing({})}
+                disabled={!Object.keys(table.getState().columnSizing).length}
+              >
+                {t("tables.reset_column_sizes")}
+              </Button>
+            </div>
+          </>
+        );
+      }}
     </PopOver>
   );
 };
