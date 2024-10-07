084dede7e8d678ecc1e48878b0057c70a441a89c
TPReal <tpreal@gmail.com>
2024-10-07 23:32:56 +0200

    Persist the selected client group on client details in history.

--- resources/js/features/client/ClientGroups.tsx
+++ resources/js/features/client/ClientGroups.tsx
@@ -1,4 +1,5 @@
 import {createQuery} from "@tanstack/solid-query";
+import {createHistoryPersistence} from "components/persistence/history_persistence";
 import {Button} from "components/ui/Button";
 import {Capitalize} from "components/ui/Capitalize";
 import {StandaloneFieldLabel} from "components/ui/form/FieldLabel";
@@ -63,6 +64,13 @@ export const ClientGroups: VoidComponent<Props> = (props) => {
   );
 
   const [selectedGroupId, setSelectedGroupId] = createSignal<string | undefined>();
+  createHistoryPersistence({
+    key: "ClientGroups",
+    value: () => ({selectedGroupId: selectedGroupId()}),
+    onLoad: (value) => {
+      setSelectedGroupId(value.selectedGroupId);
+    },
+  });
   createComputed(() => {
     if (!groupIds().length) {
       setSelectedGroupId(undefined);
