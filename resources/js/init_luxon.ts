6e334194227697c6aa0ea9a31fd0bc51976fe8e1
TPReal <tpreal@gmail.com>
2024-10-18 23:41:45 +0200

    Use the on:event syntax where passive:false needs to be specified.

---
+++ resources/js/init_luxon.ts
@@ -0,0 +1,19 @@
+import {DateTime, Duration, Settings} from "luxon";
+import {getWeekInfo} from "./components/utils";
+
+declare module "luxon" {
+  interface TSSettings {
+    throwOnInvalid: true;
+  }
+}
+
+Settings.throwOnInvalid = true;
+
+Settings.defaultWeekSettings = getWeekInfo(new Intl.Locale(navigator.language));
+
+// Prevent the luxon classes from serialising as strings, to make it possible to write a custom
+// serialiser that retains the class information.
+for (const cl of [DateTime, Duration]) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  delete (cl as any).prototype.toJSON;
+}
