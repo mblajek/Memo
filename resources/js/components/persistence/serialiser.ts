e9abbb0f481982b0621d53d91ca5ba5ddc860762
TPReal <tpreal@gmail.com>
2024-11-25 18:14:17 +0100

    Some features of the rich JSON serialiser, and unit tests.

--- resources/js/components/persistence/serialiser.ts
+++ resources/js/components/persistence/serialiser.ts
@@ -26,6 +26,80 @@ export type RichJSONValue = JSONValue<
   ReadonlyMap<RichJSONValue, RichJSONValue> | ReadonlySet<RichJSONValue> | DateTime | Duration | Interval | DaysRange
 >;
 
+const CLASS_KEY = "__rjs";
+
+function richJSONToPlainOneLevel<U>(value: RichJSONValue, valueOnPlain: U) {
+  if (value && typeof value === "object") {
+    if (value instanceof Map) {
+      return {[CLASS_KEY]: "Map", value: [...value]};
+    } else if (value instanceof Set) {
+      return {[CLASS_KEY]: "Set", value: [...value]};
+    } else if (value instanceof DateTime) {
+      return {[CLASS_KEY]: "DateTime", value: value.toISO()};
+    } else if (value instanceof Duration) {
+      return {[CLASS_KEY]: "Duration", value: value.toISO()};
+    } else if (value instanceof Interval) {
+      return {[CLASS_KEY]: "Interval", value: value.toISO()};
+    } else if (value instanceof DaysRange) {
+      return {[CLASS_KEY]: "DaysRange", value: {start: value.start.toISODate(), end: value.end.toISODate()}};
+    }
+  }
+  return valueOnPlain;
+}
+
+function richJSONFromPlainOneLevel<U>(
+  value: JSONValue | Readonly<{[key: string]: RichJSONValue}>,
+  valueOnPlain: U,
+): RichJSONValue | U {
+  if (value && typeof value === "object" && Object.hasOwn(value, CLASS_KEY) && Object.hasOwn(value, "value")) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const {[CLASS_KEY]: classKey, value: storedValue} = value as {[CLASS_KEY]: string; value: any};
+    switch (classKey) {
+      case "Map":
+        return new Map(storedValue);
+      case "Set":
+        return new Set(storedValue);
+      case "DateTime":
+        return DateTime.fromISO(storedValue);
+      case "Duration":
+        return Duration.fromISO(storedValue);
+      case "Interval":
+        return Interval.fromISO(storedValue);
+      case "DaysRange":
+        return new DaysRange(DateTime.fromISO(storedValue.start), DateTime.fromISO(storedValue.end));
+    }
+  }
+  return valueOnPlain;
+}
+
+export namespace richJSONConverter {
+  export function toPlain(value: RichJSONValue): JSONValue {
+    const valueAsPlain = richJSONToPlainOneLevel(value, undefined);
+    if (valueAsPlain === undefined) {
+      if (Array.isArray(value)) {
+        return value.map(toPlain);
+      } else if (value && typeof value === "object") {
+        return Object.fromEntries(Object.entries(value).map(([key, value]) => [key, toPlain(value)]));
+      } else {
+        return value;
+      }
+    } else {
+      return toPlain(valueAsPlain);
+    }
+  }
+
+  export function fromPlain(value: JSONValue): RichJSONValue {
+    if (Array.isArray(value)) {
+      return value.map(fromPlain);
+    } else if (value && typeof value === "object") {
+      const plainValue = Object.fromEntries(Object.entries(value).map(([key, value]) => [key, fromPlain(value)]));
+      return richJSONFromPlainOneLevel(plainValue, plainValue);
+    } else {
+      return value;
+    }
+  }
+}
+
 /**
  * A JSON serialiser that supports also some common classes. More classes can be added as needed.
  *
@@ -33,47 +107,9 @@ export type RichJSONValue = JSONValue<
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify.
  */
 export function richJSONSerialiser<T extends RichJSONValue>(): Serialiser<T> {
-  const CLASS_KEY = "__rjs";
-
   return {
-    serialise: (value: T) =>
-      JSON.stringify(value, (_key, value) => {
-        if (value instanceof Map) {
-          return {[CLASS_KEY]: "Map", value: [...value]};
-        } else if (value instanceof Set) {
-          return {[CLASS_KEY]: "Set", value: [...value]};
-        } else if (value instanceof DateTime) {
-          return {[CLASS_KEY]: "DateTime", value: value.toISO()};
-        } else if (value instanceof Duration) {
-          return {[CLASS_KEY]: "Duration", value: value.toISO()};
-        } else if (value instanceof Interval) {
-          return {[CLASS_KEY]: "Interval", value: value.toISO()};
-        } else if (value instanceof DaysRange) {
-          return {[CLASS_KEY]: "DaysRange", value: {start: value.start.toISODate(), end: value.end.toISODate()}};
-        } else {
-          return value;
-        }
-      }),
-    deserialise: (value: string) =>
-      JSON.parse(value, (_key, value) => {
-        if (value && typeof value === "object" && Object.hasOwn(value, CLASS_KEY) && Object.hasOwn(value, "value")) {
-          switch (value[CLASS_KEY]) {
-            case "Map":
-              return new Map(value.value);
-            case "Set":
-              return new Set(value.value);
-            case "DateTime":
-              return DateTime.fromISO(value.value);
-            case "Duration":
-              return Duration.fromISO(value.value);
-            case "Interval":
-              return Interval.fromISO(value.value);
-            case "DaysRange":
-              return new DaysRange(DateTime.fromISO(value.value.start), DateTime.fromISO(value.value.end));
-          }
-        }
-        return value;
-      }),
+    serialise: (value: T) => JSON.stringify(value, (_key, value) => richJSONToPlainOneLevel(value, value)),
+    deserialise: (value: string) => JSON.parse(value, (_key, value) => richJSONFromPlainOneLevel(value, value)),
     version: [2],
   };
 }
