120f4486514418bf16d0729dc6e3a52de9ef308f
TPReal <tpreal@gmail.com>
2024-12-02 16:16:26 +0100

    Removed the simple iterator polyfill for safari because it didn't work.

--- resources/js/init_types.ts
+++ resources/js/init_types.ts
@@ -1,5 +1,3 @@
-import {DEV} from "solid-js";
-
 export {};
 
 declare global {
@@ -8,29 +6,3 @@ declare global {
     isArray<T>(arg: ReadonlyArray<T> | unknown): arg is ReadonlyArray<T>;
   }
 }
-
-// Install polyfills for Iterator methods which are not supported in Safari.
-// Safari is not an officially supported browser, but it seems to be easy to stop it from failing with this change.
-// https://caniuse.com/mdn-javascript_builtins_iterator_filter
-// TODO: Remove when all major browsers support these methods.
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-function installIteratorMethod(func: keyof Array<unknown>, returnsIterator = false) {
-  if (DEV || !(Iterator.prototype as any)[func]) {
-    (Iterator.prototype as any)[func] = function (this: Array<any>, ...params: any[]) {
-      const arr = [...this];
-      const val = (arr[func] as any).apply(arr, params);
-      return returnsIterator ? val[Symbol.iterator]() : val;
-    } as any;
-  }
-}
-
-installIteratorMethod("filter", true);
-installIteratorMethod("map", true);
-installIteratorMethod("flatMap", true);
-installIteratorMethod("forEach");
-installIteratorMethod("some");
-installIteratorMethod("every");
-installIteratorMethod("find");
-installIteratorMethod("reduce");
