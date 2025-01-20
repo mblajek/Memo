793429990115b302ec44c79239b9c68bf4202196
TPReal <tpreal@gmail.com>
2025-01-20 14:41:56 +0100

    Improvements in the test page.

--- resources/js/dev-pages/TestPage.tsx
+++ resources/js/dev-pages/TestPage.tsx
@@ -1,7 +1,10 @@
+import {Button} from "components/ui/Button";
+import {Recreator} from "components/utils/Recreator";
 import {AppTitlePrefix} from "features/root/AppTitleProvider";
-import {VoidComponent} from "solid-js";
+import {createSignal, VoidComponent} from "solid-js";
 
 export const TestComponent: VoidComponent = () => {
+  console.info("%cTestComponent create", "background: white; color: green; padding: 5px; border-radius: 5px;");
   // TEST CODE HERE:
 
   return (
@@ -15,15 +18,21 @@ export default (() => {
   // During development, this place can be used to create a fake page and test components.
   // It is available at /dev/test-page, also via the "Test page" link in the menu (DEV mode only).
   // Don't submit the changes to this file.
+  const [recreatorSignal, setRecreatorSignal] = createSignal(1);
   return (
     <>
       <AppTitlePrefix prefix="TestPage" />
-      <div class="flex flex-col gap-1">
-        <div class="w-fit bg-purple-100 m-2 p-4 rounded-md">
-          Test your components here during development by creating a test page in TestPage.tsx.
+      <div class="w-full flex flex-col items-stretch gap-1">
+        <div class="bg-purple-100 m-2 p-4 rounded-md flex items-center justify-between">
+          <div>Test your components here during development by creating a test page in TestPage.tsx.</div>
+          <Button class="secondary small !bg-white" onClick={[setRecreatorSignal, recreatorSignal() + 1]}>
+            Reload
+          </Button>
         </div>
         <div class="p-2 flex flex-col items-start gap-1">
-          <TestComponent />
+          <Recreator signal={recreatorSignal()}>
+            <TestComponent />
+          </Recreator>
         </div>
       </div>
     </>
