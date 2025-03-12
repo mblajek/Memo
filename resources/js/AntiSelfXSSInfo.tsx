b74e12a19ac598a2da29f6b76aabf373f8903b3a
TPReal <tpreal@gmail.com>
2025-03-12 15:50:47 +0100

    Fixed accidentally left lint warnings.

--- resources/js/AntiSelfXSSInfo.tsx
+++ resources/js/AntiSelfXSSInfo.tsx
@@ -3,13 +3,19 @@ import {useLangFunc} from "components/utils/lang";
 import {translationsLoadedPromise} from "i18n_loader";
 import {VoidComponent} from "solid-js";
 
-export const AntiSelfXSSInfo: VoidComponent = (props) => {
+export const AntiSelfXSSInfo: VoidComponent = () => {
   const t = useLangFunc();
   setTimeout(async () => {
     await translationsLoadedPromise;
     if (!isDEV()) {
-      console.log("%c%s", "color: red; font-size: 40px;", t("anti_self_xss.header"));
-      console.log("%c%s", "font-size: 18px;", t("anti_self_xss.message"));
+      // eslint-disable-next-line no-console
+      console.log(
+        "%c%s\n%c%s",
+        "color: red; font-size: 40px;",
+        t("anti_self_xss.header"),
+        "font-size: 18px;",
+        t("anti_self_xss.message"),
+      );
     }
   }, 1500);
   return <></>;
