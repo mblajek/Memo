478d16a0f2a2a55b95797ea9e0fd1d1e46f521f9
TPReal <tpreal@gmail.com>
2025-03-12 14:47:03 +0100

    Move fonts loading from CSS to link directive in the head.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
     <link rel="icon" href="/favicon.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=PT+Mono&display=swap" rel="stylesheet">
   </head>
   <body class="antialiased overflow-hidden">
     <div id="root" class="w-dvw h-dvh"></div>
