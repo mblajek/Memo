4ba7a16e903dde10b2ebb4961ed10612edd4d01d
mblajek <mirkles@o2.pl>
2025-03-15 14:37:49 +0100

    middleware cleanup

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,32 +3,41 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Cookie\CookieValuePrefix;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpFoundation\Response;
 
 class VerifyCsrfToken extends Middleware
 {
+    private const string HEADER_REQUEST = 'X-CSRF-TOKEN';
+    private const string HEADER_RESPONSE = 'X-SET-CSRF-TOKEN';
+
     // Send the token using a header instead of a cookie.
     protected $addHttpCookie = false;
 
-    public function handle($request, Closure $next)
+    /**
+     * @param Request $request
+     * @throws TokenMismatchException
+     */
+    public function handle($request, Closure $next): Response
     {
         $response = parent::handle($request, $next);
         $this->addHeaderToResponse($request, $response);
         return $response;
     }
 
-    protected function addHeaderToResponse($request, $response)
+    protected function addHeaderToResponse(Request $request, Response $response): void
     {
-        if ($response instanceof Responsable) {
-            $response = $response->toResponse($request);
-        }
-        $response->headers->set('X-SET-CSRF-TOKEN', csrf_token());
+        $response->headers->set(
+            self::HEADER_RESPONSE,
+            $this->encrypter->encrypt($request->session()->token()),
+        );
     }
 
-    protected function getTokenFromRequest($request)
+    protected function getTokenFromRequest($request): ?string
     {
-        return $request->header('X-CSRF-TOKEN');
+        $token = $request->header(self::HEADER_REQUEST);
+        return is_string($token) ? $this->encrypter->decrypt($token) : null;
     }
-
 }
