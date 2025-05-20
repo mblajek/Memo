20f83277c4007272545693e8e42aaada2a581987
TPReal <tpreal@gmail.com>
2025-05-20 15:26:24 +0200

    Better handling of missing parts of system status.

--- resources/js/data-access/memo-api/resources/SystemStatusResource.ts
+++ resources/js/data-access/memo-api/resources/SystemStatusResource.ts
@@ -2,10 +2,10 @@ export interface SystemStatusResource {
   readonly version: string;
   readonly randomUuid: string;
   readonly currentDate: string;
-  readonly commitHash?: string;
-  readonly commitDate?: string;
+  readonly commitHash: string | null;
+  readonly commitDate: string | null;
   /** The CPU average load over the last 15 minutes. */
-  readonly cpu15m: number;
+  readonly cpu15m: number | null;
   readonly appEnv: string;
   readonly appEnvColor: string | null;
   readonly appEnvFgColor: string | null;
