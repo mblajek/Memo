315824b8219461e7fd17ed59113c6fa98f8e4e76
TPReal <tpreal@gmail.com>
2025-07-10 19:53:49 +0200

    Added CSP style attribute hashes because firefox complains about the toast icons.

--- app/Utils/Csp/ContentPolicy.php
+++ app/Utils/Csp/ContentPolicy.php
@@ -20,6 +20,17 @@ class ContentPolicy extends Basic
         $this->addDirective(Directive::IMG, 'data:');
         $this->addDirective(Directive::OBJECT, 'self');
 
+        // Firefox reports CSP violation originating from the toast library. The reason for this
+        // problem is unknown.
+        // TODO: Investigate and find a different fix.
+        $this->addDirective(Directive::STYLE_ATTR, [
+            'unsafe-hashes',
+            'sha256-X+zrZv/IbzjZUnhsbWlsecLbwjndTpG0ZynXOif7V+k=',
+            'sha256-a4ayc/80/OGda4BO/1o/V0etpOqiLx1JwB5S3beHW0s=',
+            'sha256-GVgeJ9587QD/HOULIEfnpWfHaxy666vl7wP3wwF7tbc=',
+            'sha256-eF8+x+sy8wuQzQ/PNlfTiLX/Qpfy+XFv9m6bacBd3Qk=',
+        ]);
+
         if (App::hasDebugModeEnabled()) {
             $this->addDirective(Directive::CONNECT, '*');
         } else {
